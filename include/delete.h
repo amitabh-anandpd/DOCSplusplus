5539512c51dda42a8aeeee4895e887fed4d02f6b
Amitabh Anand <amitabh.anandpd@gmail.com>
2025-11-05 15:18:07 +0530

    DELETE command implemented

---
+++ include/delete.h
@@ -0,0 +1,6 @@
+#ifndef DELETE_H
+#define DELETE_H
+
+void delete_file(int client_sock, const char* filename);
+
+#endif
---
+++ src/server/delete.c
@@ -0,0 +1,24 @@
+#include "../../include/common.h"
+#include "../../include/delete.h"
+
+int delete_from_storage(const char *filename) {
+    if (filename == NULL || filename[0] == '\0') {
+        fprintf(stderr, "delete_from_storage: filename not provided\n");
+        return -1;
+    }
+
+    char filepath[512];
+    int written = snprintf(filepath, sizeof(filepath), "%s/%s", STORAGE_DIR, filename);
+
+    if (written < 0 || written >= (int)sizeof(filepath)) {
+        fprintf(stderr, "delete_from_storage: path too long\n");
+        return -1;
+    }
+
+    if (unlink(filepath) == -1) {
+        perror("delete_from_storage: unlink failed");
+        return -1;
+    }
+
+    return 0;
+}
