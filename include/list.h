25c8f9ab831148a3ddb26d7d03770359dbf62b83
Amitabh Anand <amitabh.anandpd@gmail.com>
2025-11-19 19:59:51 +0530

    Moved list command from SS to NM

--- include/list.h
+++ include/list.h
@@ -2,6 +2,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-void list_users(int client_sock);
+void list_users(int client_sock, const char *username, const char *client_ip, unsigned short client_port);
 
 #endif
--- src/name_server/list.c
+++ src/name_server/list.c
@@ -4,15 +4,18 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-#include "../include/common.h"  // adjust path if needed
+#include "../../include/common.h"
+#include "../../include/logger.h"
 
 // List all users from storage/users.txt
-void list_users(int client_sock) {
+void list_users(int client_sock, const char *username, const char *client_ip, unsigned short client_port) {
+    log_event(LOG_INFO, "LIST command from user '%s' at IP=%s:%u", username, client_ip, client_port);
     FILE *fp;
     char line[256];
 
     fp = fopen("storage/users.txt", "r");
     if (!fp) {
+        log_event(LOG_ERROR, "Failed to open storage/users.txt for LIST command");
         const char *msg = "Error: Could not open users file.\n";
         send(client_sock, msg, strlen(msg), 0);
         return;
@@ -21,7 +24,7 @@ void list_users(int client_sock) {
     // Optional header
     const char *header = "Registered users:\n";
     send(client_sock, header, strlen(header), 0);
-
+    int user_count = 0;
     while (fgets(line, sizeof(line), fp)) {
         // Remove newline
         line[strcspn(line, "\n")] = 0;
@@ -41,7 +44,9 @@ void list_users(int client_sock) {
         char out[300];
         snprintf(out, sizeof(out), "--> %s\n", line);
         send(client_sock, out, strlen(out), 0);
+        user_count++;
     }
 
     fclose(fp);
+    log_event(LOG_INFO, "LIST command completed: sent %d users to '%s'", user_count, username);
 }
