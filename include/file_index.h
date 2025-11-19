eae7f00037cb857aa0e833abca5933ff00df1d8d
Amitabh Anand <amitabh.anandpd@gmail.com>
2025-11-19 22:58:25 +0530

    INFO command switched to NM from SS

--- include/file_index.h
+++ include/file_index.h
@@ -2,13 +2,19 @@
 #define FILE_INDEX_H
 
 #include <stddef.h>
-
+#include <time.h>
 #define MAX_SS 32
 
 typedef struct FileMeta {
     char name[256];
     int ss_ids[MAX_SS];
     int ss_count;
+    char owner[64];
+    time_t created_time;
+    time_t last_modified;
+    time_t last_accessed;
+    char read_users[512];   // comma-separated usernames
+    char write_users[512];  // comma-separated usernames
     struct FileMeta *next;
 } FileMeta;
 
@@ -23,5 +29,6 @@ FileMeta *file_index_get(FileIndex *index, const char *name);
 void file_index_put(FileIndex *index, const char *name, int ss_id);
 void file_index_remove(FileIndex *index, const char *name, int ss_id);
 void file_index_iter(FileIndex *index, void (*cb)(FileMeta *, void *), void *user);
+unsigned long hash_filename(const char *str);
 
 #endif // FILE_INDEX_H
--- src/name_server/file_index.c
+++ src/name_server/file_index.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static unsigned long hash_filename(const char *str) {
+unsigned long hash_filename(const char *str) {
     unsigned long hash = 5381;
     int c;
     while ((c = *str++)) hash = ((hash << 5) + hash) + c;
