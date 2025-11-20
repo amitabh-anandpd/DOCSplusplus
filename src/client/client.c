f3b90d8da38b6bbfed7caa92661873760a6ca338
Amitabh Anand <amitabh.anandpd@gmail.com>
2025-11-20 19:22:54 +0530

    NM IP updated in client

--- src/client/client.c
+++ src/client/client.c
@@ -60,7 +60,7 @@ int main() {
         
         server_addr.sin_family = AF_INET;
         server_addr.sin_port = htons(NAME_SERVER_PORT);
-        server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        server_addr.sin_addr.s_addr = inet_addr(NAME_SERVER_IP);
         
         if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
             perror("Connection to Name Server failed");
